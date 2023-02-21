c8c686516841269a81cb06d07fab9ad8088399ab
devendravariya <57860008+devendravariya@users.noreply.github.com>
2023-02-21 15:59:43 +0530

    added-security-group

--- Ec2/ec2.tf
+++ Ec2/ec2.tf
@@ -5,7 +5,7 @@ module "ec2_instance" {
     instance_type = var.instance_type
     key_name = resource.aws_key_pair.kp.key_name
     subnet_id = module.vpc.public_subnets[0]
-    vpc_security_group_ids = [aws_security_group.bitbucket_sg.id, aws_security_group.ssh.id]
+    vpc_security_group_ids = ["aws_security_group.bitbucket_sg.id", "aws_security_group.cloudflare.id", "aws_security_group.jenkins_sg.id"]
     associate_public_ip_address = true
     monitoring = true
     root_block_device = [
@@ -36,6 +36,3 @@ resource "aws_eip" "eip" {
     }
 }
 
-output "e2_instance_id" {
-    value = module.ec2_instance.this_instance_id
-}
