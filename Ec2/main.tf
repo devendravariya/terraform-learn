add4c24ccd0f06670818abe55ed70642ba4fc7ef
devendravariya <57860008+devendravariya@users.noreply.github.com>
2023-02-22 16:47:19 +0530

    chnages

--- Ec2/main.tf
+++ Ec2/main.tf
@@ -129,32 +129,20 @@ resource "local_file" "ssh_key" {
 }
 
 
-module "ec2_instance" {
-    source = "terraform-aws-modules/ec2-instance/aws"
-    name = var.instance_name
-    ami = var.ami
-    instance_type = var.instance_type
-    key_name = aws_key_pair.kp.key_name
-    subnet_id = module.vpc.public_subnets[0]
-    vpc_security_group_ids = [aws_security_group.ssh.id, aws_security_group.bitbucket_ips.id, aws_security_group.cloudflare_sg.id]
-    associate_public_ip_address = true
-    monitoring = true
-    root_block_device = [
-        {
-        volume_type = "gp2"
-        volume_size = 8
-        delete_on_termination = true
-        }
-    ]
-    user_data = <<EOF
+resource "aws_instance" "web" {
+  ami = var.ami
+  instance_type = var.instance_type
+  key_name = aws_key_pair.kp.key_name
+  vpc_security_group_ids = [aws_security_group.ssh.id, aws_security_group.bitbucket_ips.id, aws_security_group.cloudflare_sg.id]
+  subnet_id = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
+  user_data = <<EOF
 #!/bin/bash
 apt-get update
 apt-get install -y apache2
-echo "Hello, World from terraform!" > /var/www/html/index.html
-
 EOF
-    tags = {
-        Name = "bitbucket"
-        Environment = "dev"
-    }
+  tags = {
+    Name = "web"
+    Environment = "dev"
+  }
 }
