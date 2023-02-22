23980cad1a2e58793a4c6309bddde7c97244b7b3
devendravariya <57860008+devendravariya@users.noreply.github.com>
2023-02-22 16:16:49 +0530

    chnages

--- Ec2/ec2.tf
+++ Ec2/ec2.tf
@@ -6,9 +6,9 @@ module "ec2_instance" {
     key_name = resource.aws_key_pair.kp.key_name
     subnet_id = module.vpc.public_subnets[0]
     vpc_security_group_ids = [
-        aws_security_group.bitbucket_sg.id,
+        aws_security_group.ssh.id,
         aws_security_group.cloudflare.id,
-        aws_security_group.jenkins_sg.id,
+        aws_security_group.bitbucket.id,
     ]
     associate_public_ip_address = true
     monitoring = true
