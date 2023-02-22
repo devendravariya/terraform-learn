401e6e8a5456ab858cd6ee235df709f0ebd9f851
devendravariya <57860008+devendravariya@users.noreply.github.com>
2023-02-22 16:18:23 +0530

    chnages

--- Ec2/ec2.tf
+++ Ec2/ec2.tf
@@ -5,11 +5,7 @@ module "ec2_instance" {
     instance_type = var.instance_type
     key_name = resource.aws_key_pair.kp.key_name
     subnet_id = module.vpc.public_subnets[0]
-    vpc_security_group_ids = [
-        aws_security_group.ssh.id,
-        aws_security_group.cloudflare.id,
-        aws_security_group.bitbucket.id,
-    ]
+    vpc_security_group_ids = [aws_security_group.ssh.id, aws_security_group.bitbucket.id, aws_security_group.cloudflare_sg.id]
     associate_public_ip_address = true
     monitoring = true
     root_block_device = [
